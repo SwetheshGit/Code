573898013e0ba43cfef5c94737f8ae6608e64413
SwetheshGit <swetheshmuddina2001@gmail.com>
2025-03-14 10:11:58 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App {
     @GetMapping("/")
     public String home(Model model) {
         // Pass the header text to the view
-        model.addAttribute("header", "Cloud Computing in Telugu Swethesh2ndchange");
+        model.addAttribute("header", "Cloud Computing in Telugu Swethesh_Early morning change");
         return "index"; // Return the index.html template
     }
 }
